35e02a4eecd7035edd3f9d3a9ac9cde34c9961ac
dhawy-07 <dhawy@mhs.usk.ac.id>
2025-11-16 18:20:07 +0700

    Update lirik.c

--- lirik.c
+++ lirik.c
@@ -10,10 +10,18 @@ void keluar(const char *teks, int delay){
 }
 
 int main(){
-    keluar("I got my driver's license last week\n",120);
-        Sleep(4000);
+    keluar("I got my driver's license last week\n",90);
+        Sleep(1500);
     keluar("just like we always talked about\n",40);
         Sleep(3000);
-    keluar("'cause you to excited for me\n", 20);
+    keluar("'Cause you to excited for me\n", 20);\
+        Sleep(2000);
+    keluar("To finally drive up to your house\n", 20);\
+        Sleep(2000);
+    keluar("But today i drove through the surbubs\n", 20);\
+        Sleep(2000);
+    keluar("Cryin 'cause you weren't around\n", 20);\
+        Sleep(2000);
 return 0;
+
 }
