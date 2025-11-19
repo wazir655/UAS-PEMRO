e55482e12c60eaabdf572bbae7d01dc3385ca436
aisyaa29 <aisyaa25@mhs.usk.ac.id>
2025-11-19 21:32:36 +0700

    Update uas.c

--- uas.c
+++ uas.c
@@ -53,3 +53,43 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
+int login() {
+    FILE *fp = fopen("akun.txt", "r");
+    if (!fp) {
+        printf("File akun.txt tidak ditemukan!\n");
+        return 0;
+    }
+
+    Akun a;
+    while (fscanf(fp, "%s %s %s", a.username, a.password, a.role) != EOF) {
+        if (strcmp(a.username, loginUser.username) == 0 && strcmp(a.password, loginUser.password) == 0) {
+            strcpy(loginUser.role, a.role);
+            fclose(fp);
+            return 1;
+        }
+    }
+    fclose(fp);
+    return 0;
+}
+
+void menuAdmin() {
+    int pilih;
+    do {
+        printf("\n=== MENU ADMIN ===\n");
+        printf("1. Lihat daftar alat\n");
+        printf("2. Tambah alat\n");
+        printf("3. Edit alat\n");
+        printf("4. Hapus alat\n");
+        printf("0. Keluar\n");
+        printf("Pilih: ");
+        scanf("%d", &pilih);
+
+        switch (pilih) {
+            case 1: lihatAlat(); break;
+            case 2: tambahAlat(); break;
+            case 3: editAlat(); break;
+            case 4: hapusAlat(); break;
+        }
+    } while (pilih != 0);
+}
